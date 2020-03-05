487660056194ead54091dec86c0b80f04bff3e7d
ampgular <ampgular@gmail.com>
2020-03-05 20:45:33 +0100

    init

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './epos-epson.common';
+export declare class EposEpson extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
