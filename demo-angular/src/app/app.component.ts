70d7ddc487d3d4ce49b7548448e37d4811971bc3
ampGular <ampgular@gmail.com>
2020-03-06 14:21:43 +0100

    initial commit

---
+++ demo-angular/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { EposEpson } from 'nativescript-epos-epson';
+console.log(new EposEpson().message);
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ns-app",
+    templateUrl: "app.component.html"
+})
+export class AppComponent { }
