0e874313310c2b9a846e255b67655649a89be67d
ampgular <ampgular@gmail.com>
2020-05-23 09:31:07 +0200

    okdk

--- demo-angular/src/app/home/home.component.ts
+++ demo-angular/src/app/home/home.component.ts
@@ -1,16 +1,30 @@
 import { Component, OnInit } from "@angular/core";
+import { android  } from "tns-core-modules/application";
+
+declare let com: any;
+
+
+export class epsonPrinter {}
 
 @Component({
     selector: "Home",
     templateUrl: "./home.component.html"
 })
 export class HomeComponent implements OnInit {
+    context: any;
 
     constructor() {
+        this.context = android.context;
         // Use the component constructor to inject providers.
     }
 
     ngOnInit(): void {
+
+        const printerold = new epsonPrinter();
+
+        const printer = new com.epson.epos2.printer.Printer(com.epson.epos2.printer.Printer.TM_T88, com.epson.epos2.printer.Printer.MODEL_ANK, this.context );
+       console.log('5)')
+        console.log(printer)
         // Init your component properties here.
     }
 }
