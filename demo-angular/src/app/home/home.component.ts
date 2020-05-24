5b2ffc14c7f0fb1d0c22dc54fa3f56bbd202e9b0
ampgular <ampgular@gmail.com>
2020-05-24 11:03:22 +0200

    okdk

--- demo-angular/src/app/home/home.component.ts
+++ demo-angular/src/app/home/home.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from "@angular/core";
 import { android  } from "tns-core-modules/application";
 
 declare let com: any;
-
+// declare let  EPOS_Exveption = com.epson.epos2.Epos2Exception;
+// declare let  EPOS_Printer = com.epson.epos2.printer.Printer
+// declare let  com.epson.epos2.Log;
+// declare let  com.epson.epos2.discovery.DeviceInfo;
+// declare let  com.epson.epos2.discovery.Discovery;
+// declare let  com.epson.epos2.discovery.DiscoveryListener;
+// declare let  com.epson.epos2.discovery.FilterOption;
 
 export class epsonPrinter {}
 
@@ -18,13 +24,57 @@ export class HomeComponent implements OnInit {
         // Use the component constructor to inject providers.
     }
 
+
+
+
     ngOnInit(): void {
 
-        const printerold = new epsonPrinter();
+        let listen = new com.epson.epos2.discovery.DiscoveryListener( (device)=> {
+            console.log(device)
+        })
+        console.log(35)
+        try {
 
-        const printer = new com.epson.epos2.printer.Printer(com.epson.epos2.printer.Printer.TM_T88, com.epson.epos2.printer.Printer.MODEL_ANK, this.context );
+        const printer = new com.epson.epos2.printer.Printer(com.epson.epos2.printer.Printer.TM_M30, com.epson.epos2.printer.Printer.MODEL_ANK, this.context );
        console.log('5)')
         console.log(printer)
-        // Init your component properties here.
+        console.log(6);
+
+       printer.setReceiveEventListener(this.context);
+        //   printer.setStatusChangeEventListener(this.context);
+            console.log(7);
+        //     printer.setInterval(3000);
+        //     printer.startMonitor();
+
+
+
+
+            printer.addTextAlign(com.epson.epos2.printer.Printer.ALIGN_CENTER);
+           printer.addText("Hello World");
+           console.log(8);
+
+           printer.connect("TCP:192.168.0.16", com.epson.epos2.printer.Printer.PARAM_DEFAULT);
+
+           printer.addTextAlign(com.epson.epos2.printer.Printer.ALIGN_CENTER);
+           printer.addText("Hello World");
+           printer.beginTransaction();
+           printer.addText("Hello World!\n");
+           printer.sendData(com.epson.epos2.printer.Printer.PARAM_DEFAULT);
+
+        } catch (error) {
+            console.log(error)
+            const b = error
+
+
+
+       console.log(error)
+
+
+        }
+
+
+    }
+    mDiscoveryListener(){
+        console.log('hola')
     }
 }
